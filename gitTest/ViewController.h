f84e214d69ef89c21c30f005ea5d72e65e265c20
Viktor Todorov <vikt0r40@icloud.com>
2014-03-14 10:46:24 +0200

    Initial Commit

---
+++ gitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by Viktor Todorov on 3/14/14.
+//  Copyright (c) 2014 Viktor Todorov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ gitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by Viktor Todorov on 3/14/14.
+//  Copyright (c) 2014 Viktor Todorov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
