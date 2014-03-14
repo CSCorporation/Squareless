93cd84f1d446203babfd758b48bae4b190554b0b
Viktor Todorov <vikt0r40@icloud.com>
2014-03-14 10:49:51 +0200

    Hello Again Saho

--- gitTest/ViewController.m
+++ gitTest/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Hello From Git Saho");
+    NSLog(@"Hello Again After Commit");
 }
 
 - (void)didReceiveMemoryWarning
